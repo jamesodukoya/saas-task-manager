b7f833c38676d030d70c77f0c49740a062677e1b
jamesodukoya <odukoyaoluwadara@gmail.com>
2024-09-03 22:06:10 +0100

    Add navigation bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import { Banner } from "@/components/Banner";
+import { Navbar } from "@/components/Navbar";
 
 
 export default function Home() {
   return (
-    <Banner />
+    <>
+      <Banner />
+      <Navbar />
+    </>
   );
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,6 @@
 export const Banner = () => {
-  return <div className="py-3 text-center bg-[linear-gradient(to_right,rgb(252,_214,_255,_.7),rgb(41,_216,_255,_.7),rgb(255,_253,_128,_.7),rgb(248,_154,_191,_.7),rgb(252,_214,_255,_.7))]">
+  return (
+    <div className="py-3 text-center bg-[linear-gradient(to_right,rgb(252,_214,_255,_.7),rgb(41,_216,_255,_.7),rgb(255,_253,_128,_.7),rgb(248,_154,_191,_.7),rgb(252,_214,_255,_.7))]">
     <div className="container">
       <p className="font-medium">
         <span className="hidden sm:inline">Introducing a completely redesigned interface - </span>
@@ -8,5 +9,6 @@ export const Banner = () => {
         </a>
       </p>
     </div>
-  </div>;
+  </div>
+  );
 };
